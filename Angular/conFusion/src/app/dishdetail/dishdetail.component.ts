e2de0539808891a7ea5a7c0233ce8ba590bc8be2
SHENAL1 <51831496+SHENAL1@users.noreply.github.com>
2020-05-25 15:36:45 +0530

    Single Page Applications Part 2

--- Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-dishdetail',
@@ -8,14 +12,22 @@ import { Dish } from '../shared/dish';
 })
 export class DishdetailComponent implements OnInit {
 
-    @Input()
     dish: Dish  
 
 //   comments =this.dish.comments;
   
-  constructor() { }
+  constructor(private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
-  ngOnInit() {
+  ngOnInit() { 
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
+  }
+
+  // To go back to the menu component
+  goBack(): void{
+    this.location.back();
   }
 
 }
