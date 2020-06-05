5e2e61d873755d41fdb1af923b8a8fbc26984354
SHENAL1 <51831496+SHENAL1@users.noreply.github.com>
2020-06-05 17:30:46 +0530

    HTTP Part 2

--- Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -18,6 +18,7 @@ import { from } from 'rxjs';
 export class DishdetailComponent implements OnInit {
 
     dish: Dish;
+    errMess:string;
     dishIds: string[];
     prev: string;
     next: string;  
@@ -58,7 +59,8 @@ export class DishdetailComponent implements OnInit {
 
     this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); },
+    errmess => this.errMess = <any>errmess);
   }
 
    //Slider
