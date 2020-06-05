2517db8b2a873f8db3c5863df1e9ca2cd3e7c210
SHENAL1 <51831496+SHENAL1@users.noreply.github.com>
2020-06-05 17:57:53 +0530

    Saving Changes to Server

--- Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -27,6 +27,8 @@ export class DishdetailComponent implements OnInit {
     comment: Comment;
     @ViewChild('fform') commentFormDirective;
 
+    dishcopy:Dish;
+
     formErrors = {
       'author': '',
       'comment': ''
@@ -59,7 +61,7 @@ export class DishdetailComponent implements OnInit {
 
     this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); },
+    .subscribe(dish => { this.dish = dish; this.dishcopy =dish; this.setPrevNext(dish.id); },
     errmess => this.errMess = <any>errmess);
   }
 
@@ -118,7 +120,12 @@ export class DishdetailComponent implements OnInit {
     const date = new  Date();
     this.comment.date = date.toISOString();
 
-    this.dish.comments.push(this.comment);
+    this.dishcopy.comments.push(this.comment);
+    this.dishService.putDish(this.dishcopy)
+    .subscribe(dish =>{
+      this.dish=this.dish; this.dishcopy = dish;
+    },
+    errmess => {this.dish = null; this.dishcopy =<any>errmess});
 
     console.log(this.comment);
     this.commentForm.reset({
